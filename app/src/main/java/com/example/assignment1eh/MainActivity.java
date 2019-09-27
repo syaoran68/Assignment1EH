9be378b1e7bdf7798350fd0a5bfc446d77c0d3cd
Eddie Huang <edhuang04@gmail.com>
2019-09-26 21:17:38 -0700

    tablelayout

--- app/src/main/java/com/example/assignment1eh/MainActivity.java
+++ app/src/main/java/com/example/assignment1eh/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.assignment1eh;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TableLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        TableLayout regular = new TableLayout(this);
+
+        TextView bar1 = new TextView(this);
+        bar1.setText("1");
+
         setContentView(R.layout.activity_main);
     }
 }

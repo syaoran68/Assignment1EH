b1a5793c1c4628b7846892d4a4cefa064cb27b46
Eddie Huang <edhuang04@gmail.com>
2019-09-28 11:23:05 -0700

    grid layout and starting left side

--- app/src/main/java/com/example/assignment1eh/MainActivity.java
+++ app/src/main/java/com/example/assignment1eh/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.assignment1eh;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.GridLayout;
 import android.widget.TableLayout;
 import android.widget.TextView;
 
@@ -12,11 +15,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        TableLayout regular = new TableLayout(this);
+        GridLayout regular = new GridLayout(this);
+        ViewGroup.LayoutParams rParams = new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+        );
 
-        TextView bar1 = new TextView(this);
-        bar1.setText("1");
+        regular.setLayoutParams(rParams);
+        regular.setOrientation(GridLayout.VERTICAL);
+        regular.setBackgroundColor(Color.parseColor("#787878"));
+        regular.setColumnCount(10);
+        regular.setRowCount(10);
 
-        setContentView(R.layout.activity_main);
+        //bar1
+
+        GridLayout.Spec row_spec = GridLayout.spec(0,2f);
+        GridLayout.Spec col_spec = GridLayout.spec(0,2f);
+        GridLayout.LayoutParams cParams = new GridLayout.LayoutParams(row_spec, col_spec);
+        cParams.setMargins(10,10,10,10);
+        TextView tv;
+
+
+        //bar2
+        tv = new TextView(this);
+        tv.setText("1");
+        tv.setTextSize(16);
+        regular.addView(tv);
+
+        //bar3
+        tv = new TextView(this);
+        tv.setText("2");
+        tv.setTextSize(16);
+        regular.addView(tv);
+
+
+        setContentView(regular);
     }
 }

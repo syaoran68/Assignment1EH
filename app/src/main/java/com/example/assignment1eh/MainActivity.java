1020eef630fb680f20ebfcd1319247baebe8408a
Eddie Huang <edhuang04@gmail.com>
2019-09-28 21:48:10 -0700

    Left side numbers set

--- app/src/main/java/com/example/assignment1eh/MainActivity.java
+++ app/src/main/java/com/example/assignment1eh/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.ViewGroup;
 import android.widget.GridLayout;
 import android.widget.TableLayout;
@@ -23,32 +24,77 @@ public class MainActivity extends AppCompatActivity {
 
         regular.setLayoutParams(rParams);
         regular.setOrientation(GridLayout.VERTICAL);
-        regular.setBackgroundColor(Color.parseColor("#787878"));
+        //regular.setBackgroundColor(Color.parseColor("#787878"));
         regular.setColumnCount(10);
-        regular.setRowCount(10);
+        regular.setRowCount(12);
 
         //bar1
+        for(int i = 0; i < 12; i++)
+        {
+            TextView tv;
+            tv = new TextView(this);
+            tv.setText(i + "");
+            tv.setBackgroundColor(Color.GRAY);
+            GridLayout.Spec row_spec = GridLayout.spec(i,1f);
+            GridLayout.Spec col_spec = GridLayout.spec(0,1f);
+            GridLayout.LayoutParams cParams = new GridLayout.LayoutParams(row_spec, col_spec);
+            tv.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.CENTER_VERTICAL);
+            cParams.width = 65;
+            //cParams.setMargins(10,10,10,10);
+            tv.setLayoutParams(cParams);
+            regular.addView(tv);
+        }
 
-        GridLayout.Spec row_spec = GridLayout.spec(0,2f);
-        GridLayout.Spec col_spec = GridLayout.spec(0,2f);
-        GridLayout.LayoutParams cParams = new GridLayout.LayoutParams(row_spec, col_spec);
-        cParams.setMargins(10,10,10,10);
-        TextView tv;
 
 
-        //bar2
-        tv = new TextView(this);
-        tv.setText("1");
-        tv.setTextSize(16);
-        regular.addView(tv);
 
-        //bar3
-        tv = new TextView(this);
-        tv.setText("2");
-        tv.setTextSize(16);
-        regular.addView(tv);
+
+//        TextView tv;
+//        tv = new TextView(this);
+//        tv.setText("1");
+//        tv.setBackgroundColor(Color.GRAY);
+//        GridLayout.Spec row_spec = GridLayout.spec(0,1f);
+//        GridLayout.Spec col_spec = GridLayout.spec(0,1f);
+//        GridLayout.LayoutParams cParams = new GridLayout.LayoutParams(row_spec, col_spec);
+//        tv.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.CENTER_VERTICAL);
+//        cParams.width = 50;
+//        //cParams.setMargins(10,10,10,10);
+//        tv.setLayoutParams(cParams);
+//        regular.addView(tv);
+//
+//        //bar2
+//        TextView tv1 = new TextView(this);
+//        tv1.setText("2");
+//        tv1.setBackgroundColor(Color.GRAY);
+//        tv1.setTextSize(16);
+//        GridLayout.Spec row1_spec = GridLayout.spec(1,1f);
+//        GridLayout.Spec col1_spec = GridLayout.spec(0,1f);
+//        GridLayout.LayoutParams gParams = new GridLayout.LayoutParams(row1_spec, col1_spec);
+//        tv1.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.CENTER_VERTICAL);
+//        gParams.width = 50;
+//        tv1.setLayoutParams(gParams);
+//        regular.addView(tv1);
+//
+//        //bar3
+//        TextView tv2 = new TextView(this);
+//        tv2.setText("3");
+//        tv2.setBackgroundColor(Color.GRAY);
+//        tv2.setTextSize(16);
+//        GridLayout.Spec row2_spec = GridLayout.spec(2,1f);
+//        GridLayout.Spec col2_spec = GridLayout.spec(0,1f);
+//        GridLayout.LayoutParams tv2Params = new GridLayout.LayoutParams(row2_spec, col2_spec);
+//        tv2.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.CENTER_VERTICAL);
+//        tv2Params.width = 50;
+//        tv2.setLayoutParams(tv2Params);
+//        regular.addView(tv2);
+
+
+
+
+
 
 
         setContentView(regular);
+
     }
 }
